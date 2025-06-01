7fcf35e050ad2bcb9cc8fa0519349ae57a37c9fc
Yann B. <ybycode@gmail.com>
2025-06-02 00:15:19 +0200

    Got rid of the elixir-format nvim plugin in favor of conform, + elixir formatting config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -260,6 +260,10 @@ return {
       require("conform").setup {
         formatters_by_ft = {
           lua = { "stylua" },
+          elixir = { "mix" },
+          eelixir = { "mix" },
+          heex = { "mix" },
+          surface = { "mix" },
         },
       }
 
